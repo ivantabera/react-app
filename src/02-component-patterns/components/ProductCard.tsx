70711a43dfea12189faac15245422971927fdd2d
Ivan Tabera <ivantabera19@gmail.com>
2022-01-05 16:29:10 -0600

    add fix componente basico tradicional

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -6,6 +6,7 @@ export const ProductCard = () => {
 
     const [count, setCount] = useState(0);
 
+    /* Funcion que no permite bajar del 0 */
     const increaseBy = ( value:number ) => {
         setCount(prev => Math.max( prev + value, 0 ) )
     }
