bd9a338befdb9b2a3469b96d93c7a27a213be27a
Ivan Tabera <ivantabera19@gmail.com>
2022-01-17 11:22:41 -0600

    Controlando el estado desde el padre

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -29,8 +29,23 @@ export const ShoppingPage = () => {
     const onProductCountChange = ({ count, product }: { count:number, product:IProduct }) => {
         
         setShoppingCart( oldShoppingCart => {
+
+            const productInCart:IProductInCart = oldShoppingCart[product.id] || { ...product, count:0 };
+            console.log('productInCart', productInCart)
+
+            if ( Math.max( productInCart.count + count, 0 ) > 0 ) {
+                productInCart.count += count;
+                return {
+                    ...oldShoppingCart, 
+                    [product.id]: productInCart
+                }
+            }
+
+            const { [product.id]:toDelete, ...rest} = oldShoppingCart;
+            return { ...rest };
+
             /* Eliminar el objeto cuando su count llegue a 0 */
-            if (count === 0) {
+        /*  if (count === 0) {
                 const { [product.id]:toDelete, ...rest} = oldShoppingCart;
                 return {...rest}
             }
@@ -38,7 +53,7 @@ export const ShoppingPage = () => {
             return {
                 ...oldShoppingCart,
                 [ product.id ]: { ...product, count }
-            }
+            } */
         })
     }
 
