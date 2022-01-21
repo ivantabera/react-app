654ff3cceca0bb8cbddf30a18e96728e99d5b6ae
ivantabera <57042047+ivantabera@users.noreply.github.com>
2022-01-21 15:23:53 -0600

    Formulario tradicional con useState

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,6 +1,21 @@
+import { ChangeEvent, useState } from "react";
 import "../styles/styles.css";
 
 export const RegisterPage = () => {
+
+    const [registerData, setRegisterData] = useState({
+        name:'Ivan tabera', 
+        email:'ivantabera19@gmail.com', 
+        password1:'123456', 
+        password2:'123456',
+    });
+
+    const { name, email, password1, password2 } = registerData;
+
+    const onChange = ( ev:ChangeEvent<HTMLInputElement> ) => {
+        console.log('ev', ev.target.value)
+    }
+
     return (
         <div>
             <h1>Register page</h1>
@@ -10,21 +25,29 @@ export const RegisterPage = () => {
                 <input 
                     type="text"
                     placeholder="Name"
+                    value={ name }
+                    onChange={ onChange }
                 />
                 
                 <input 
                     type="email" 
-                    placeholder="Email" 
+                    placeholder="Email"
+                    value={ email }
+                    onChange={ onChange }
                 />
 
                 <input 
                     type="password" 
-                    placeholder="Password" 
+                    placeholder="Password"
+                    value={ password1 }
+                    onChange={ onChange }
                 />
 
                 <input 
-                    type="password2" 
-                    placeholder="Repeat Password" 
+                    type="password" 
+                    placeholder="Repeat Password"
+                    value={ password2 }
+                    onChange={ onChange }
                 />
 
                 <button type="submit">Create</button>
