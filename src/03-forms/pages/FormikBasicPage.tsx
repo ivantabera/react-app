6cd750445db30f755b71b822092a7108413c05c1
ivantabera <57042047+ivantabera@users.noreply.github.com>
2022-01-24 14:13:46 -0600

    Formik - obtener informacion del formulario

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,23 +1,51 @@
+import { useFormik } from 'formik';
+
 import '../styles/styles.css';
 
 export const FormikBasicPage = () => {
+
+    const { handleChange, values, handleSubmit } = useFormik({
+        initialValues:{
+            firstName:'',
+            lastName:'',
+            email:''
+        }, 
+        onSubmit: values => {
+            console.log('values', values)
+        }
+    })
 
     return (
         <div>
             <h1>Formik Basic Tutorial</h1>
 
-            <form noValidate>
+            <form noValidate onSubmit={ handleSubmit }>
 
                 <label htmlFor="firstName">First Name</label>
-                <input type="text" name="firstName" />
+                <input 
+                    type="text" 
+                    name="firstName" 
+                    onChange={ handleChange}
+                    value={ values.firstName }
+                />
                 <span>Este campo es requerido</span>
 
                 <label htmlFor="lastName">Last Name</label>
-                <input type="text" name="lastName" />
+                <input 
+                    type="text" 
+                    name="lastName" 
+                    onChange={ handleChange}
+                    value={ values.lastName }
+                />
                 <span>Este campo es requerido</span>
 
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" />
+                <input 
+                    type="email" 
+                    name="email" 
+                    onChange={ handleChange}
+                    value={ values.email }
+                />
                 <span>Verifica el formato del email</span>
 
                 <button type="submit">Create</button>
