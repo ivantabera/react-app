f7a1a858bf5773f181537be95abab128fe667b7c
ivantabera <57042047+ivantabera@users.noreply.github.com>
2022-01-24 15:18:15 -0600

    Formik - validaciones manuales (debemos escribir bien los formatos firstname, lastname y correo para ver el resultado en consola)

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,8 +1,39 @@
-import { useFormik } from 'formik';
+import { useFormik, FormikErrors } from 'formik';
 
 import '../styles/styles.css';
 
+interface FormValues{ 
+    firstName:string, 
+    lastName:string, 
+    email:string
+}
+
 export const FormikBasicPage = () => {
+
+    const validate = ( { firstName, lastName, email }:FormValues ) => {
+        
+        const errors:FormikErrors<FormValues> = {};
+
+        if (!firstName) {
+            errors.firstName = 'Required';
+        } else if ( firstName.length >= 15 ){
+            errors.firstName = 'Must be 15 characteres or less';
+        }
+
+        if (!lastName) {
+            errors.lastName = 'Required';
+        } else if ( lastName.length >= 10 ){
+            errors.lastName = 'Must be 10 characteres or less';
+        }
+
+        if (!email) {
+            errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+            errors.email = 'Invalid email address';
+        }
+
+        return errors
+    };
 
     const { handleChange, values, handleSubmit } = useFormik({
         initialValues:{
@@ -12,7 +43,8 @@ export const FormikBasicPage = () => {
         }, 
         onSubmit: values => {
             console.log('values', values)
-        }
+        },
+        validate
     })
 
     return (
